82dfcaa831d4bc077b6244429c30fb42b029844e
WeijieLiu96 <WeijieLiu96@users.noreply.github.com>
2018-07-12 12:55:21 +0200

    12321

--- exercici/snake.js
+++ exercici/snake.js
@@ -1,5 +1,5 @@
-var snake = [{x:0,y:20}];
-var fruit =[{x:150,y:150}];
+var snake = {x:0,y:20};
+var fruit ={x:150,y:150};
 var WIDTH = 200;
 var HEIGHT = 50;
 var ctx;
@@ -10,32 +10,31 @@ var size = 10;
 var have_fruit = false;
 var DSNAKEX = 10;
 var DSNAKEY = 10;
-var his_snake = [{x:snake[0].x,y:snake[0].y}];
-var i;
+var his_snake = [{x:snake.x,y:snake.y}];
 
 function init(){
 	ctx = document.getElementById("container").getContext("2d");
 	updateFruit();
 	//need interval = setInterval(draw,1);
-	draw(snake[0].x,snake[0].y,size,"black");
+	draw(snake.x,snake.y,size,"black");
 	interval = setInterval(update,500);
 
 }
 function updateFruit(){
 	if(!have_fruit){
 		parseInt(Math.random()*((WIDTH/size-1)+0),10);
-		var x = fruit[0].x = Math.floor(Math.random()*((WIDTH/size-1)+1)+0);
-		fruit[0].x=x *=10;
+		var x = fruit.x = Math.floor(Math.random()*((WIDTH/size-1)+1)+0);
+		fruit.x=x *=10;
 		parseInt(Math.random()*((HEIGHT/size-1)+0),10);
-		var y = fruit[0].y = Math.floor(Math.random()*((HEIGHT/size-1)+1)+0);
-		fruit[0].y = y *=10;
+		var y = fruit.y = Math.floor(Math.random()*((HEIGHT/size-1)+1)+0);
+		fruit.y = y *=10;
 		have_fruit = true;
 	}
 	draw(x,y,size,"red");
 }
 function update(){
 	clear();
-	draw(fruit[0].x,fruit[0].y,size,"red");
+	draw(fruit.x,fruit.y,size,"red");
 	updatesnake();
 }
 
@@ -51,17 +50,18 @@ function draw(x,y,size,color = "black"){
 }
 
 function updatesnake(){
-	var c_snake = {x:snake[0].x,y:snake[0].y};
-	var n_snake = {x:snake[0].x,y:snake[0].y};
+	var c_snake = {x:snake.x,y:snake.y};
+	var n_snake = {x:snake.x,y:snake.y};
 	if(d_snake.mov_x!=null)n_snake.x = n_snake.x+d_snake.mov_x*size;
 	if(d_snake.mov_y!=null)n_snake.y = n_snake.y+d_snake.mov_y*size;
-	if((snake[0].x == fruit[0].x) && (snake[0].y == fruit[0].y)){
-		for(i = 0; i < length_snake-2; ++i){
-			var aux = {x:his_snake[i+1].x,y:his_snake[1].y};
-			his_snake[i] = aux;
+	if((snake.x == fruit.x) && (snake.y == fruit.y)){
+		for(var i = 0; i < length_snake-2; ++i){
+			var aux = {x:his_snake[i+1].x,y:his_snake[i+1].y};
+			his_snake[i].x = aux.x;
+			his_snake[i].y = aux.y;
 		}
-		his_snake[length_snake-1].x = snake[0].x;
-		his_snake[length_snake-1].y = snake[0].y;
+		his_snake[length_snake-1].x = snake.x;
+		his_snake[length_snake-1].y = snake.y;
 		have_fruit = false;
 		length_snake++;
 		his_snake.push({x:n_snake.x,y:n_snake.y});
@@ -71,25 +71,26 @@ function updatesnake(){
 		}
 	}
 	else if((length_snake != 1)){
-		for(i = 0; i < length_snake-2; ++i){
+		for(var i = 0; i < length_snake-2; ++i){
 			var aux = {x:his_snake[i+1].x,y:his_snake[1].y};
-			his_snake[i] = aux;
+			his_snake[i].x = aux.x;
+			his_snake[i].y = aux.y;
 		}
-		his_snake[length_snake-1].x = snake[0].x;
-		his_snake[length_snake-1].y = snake[0].y;
+		his_snake[length_snake-1].x = snake.x;
+		his_snake[length_snake-1].y = snake.y;
 		for(i = 0; i < length_snake; ++i){
 			draw(his_snake[i].x,his_snake[i].y,size,"black");
 		}
 	}
 	else {
-		draw(n_snake.x,n_snake.y,size,"black");
+		//draw(n_snake.x,n_snake.y,size,"black");
 
 		his_snake[0].x = c_snake.x;
 		his_snake[0].y = c_snake.y;
-		draw(snake[0].x,snake[0].y,size,"black");
+		draw(his_snake[0].x,his_snake[0].y,size,"black");
 	}
-	snake[0].x = n_snake.x;
-	snake[0].y = n_snake.y;
+	snake.x = n_snake.x;
+	snake.y = n_snake.y;
 	
 }
 
